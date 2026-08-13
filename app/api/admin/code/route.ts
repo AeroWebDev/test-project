25668b7831218ea38016c9c93f6994b8928a0bfd
Marwan khaled <mrwan8khaled@gmail.com>
2026-08-13 22:49:09 +0300

    fixing routes

--- app/api/admin/code/route.ts
+++ app/api/admin/code/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { createAdminClient } from "@/lib/supabase";
+import { createAdminClient } from "@/src/lib/supabase";
+
 
 export async function POST(req: Request) {
   try {
